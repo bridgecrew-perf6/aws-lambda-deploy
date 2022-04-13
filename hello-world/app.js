c07a81e560c0575760fae5b70d603ea4e5b8361e
Narendra Shanbhag <nshanbhag@conversantmedia.com>
2022-04-13 17:56:56 +0530

    Code deploy test 8

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'Testing aliasing---7',
+                message: 'Lambda using Sam CLI',
                 // location: ret.data.trim()
             })
         }
